693af48163485fdc804f9ee69083b3a502296e67
David Liebherr <ambition.aidev@gmail.com>
2024-07-13 12:25:53 -0500

    added notes in txt file... arrays and foreach

--- Program.cs
+++ Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿string[] fraudIDs = {"b123", "c84888", "b548", "564", "bs88", "asd477", "bre54"};
+
+foreach (string fraudID in fraudIDs)
+{
+   if (fraudID.StartsWith("b")){
+      Console.WriteLine(fraudID);
+   }
+}
--- obj/Debug/net8.0/c-sharp.AssemblyInfo.cs
+++ obj/Debug/net8.0/c-sharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("c-sharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b51122575c3cc5b449c39988e1336ac67b2d3cec")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2d3aae89633ae735ce17f9aafebae4ea223cf7f")]
 [assembly: System.Reflection.AssemblyProductAttribute("c-sharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("c-sharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
