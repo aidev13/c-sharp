7efb7be474a798099eedc42602f22e5139d7bcce
David Liebherr <ambition.aidev@gmail.com>
2024-07-19 17:09:04 -0500

    LOL man my code was different than the microsoft code.. still worked but wow!

--- Program.cs
+++ Program.cs
@@ -1,10 +1,1 @@
-﻿for (int i = 0; i <= 100; i++)
-{
-   if ((i % 5 == 0) && (i % 3 == 0))
-      Console.WriteLine("fizzbuzz");
-   else if (i % 5 == 0)
-      Console.WriteLine("buzz");
-   else if (i % 3 == 0)
-      Console.WriteLine("Fizz");
-   else Console.WriteLine(i);
-}
+﻿
--- obj/Debug/net8.0/c-sharp.AssemblyInfo.cs
+++ obj/Debug/net8.0/c-sharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("c-sharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d33092e1df7f7a1648a42527cfe0bd0dad7cae84")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6457ce0b364179d6606eaf1a1d21291f1ba54481")]
 [assembly: System.Reflection.AssemblyProductAttribute("c-sharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("c-sharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
