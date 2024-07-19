f501485c25fc87abba4efb7bf04d24a9e8c42d32
David Liebherr <ambition.aidev@gmail.com>
2024-07-19 07:42:54 -0500

    you know...stuff

--- Program.cs
+++ Program.cs
@@ -1,4 +1,21 @@
-﻿Random number = new();
-string call = (number.Next(1,3) == 1) ? "tails" : "heads";
+﻿//Decision logic challenge
 
-Console.WriteLine(call);
+string permission = "Manager";
+int level = 21;
+
+if (permission.Contains("Admin"))
+{
+   if (level > 55)
+   Console.WriteLine("Super User.");
+   else
+   Console.WriteLine("Admin user");
+}
+ else if (permission.Contains("Manager"))
+{
+   if(level >= 20)
+   Console.WriteLine("Contact admins.");
+   else
+   Console.WriteLine("Access denied");
+}
+else 
+Console.WriteLine("You do not have the rights/");
--- obj/Debug/net8.0/c-sharp.AssemblyInfo.cs
+++ obj/Debug/net8.0/c-sharp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("c-sharp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b89732c29fed48e17d81b2cc5594fcec9d47679")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+153ec8bbde3f1246e6509bbeb289c6b087636ea1")]
 [assembly: System.Reflection.AssemblyProductAttribute("c-sharp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("c-sharp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
